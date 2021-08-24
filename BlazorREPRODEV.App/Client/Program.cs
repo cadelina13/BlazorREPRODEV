13edcc93d2bcdd6b1bca0155072f74690dbdd813
Rio Cadeliña <cadelina13@gmail.com>
2021-08-24 11:25:07 +0800

    aa

--- BlazorREPRODEV.App/Client/Program.cs
+++ BlazorREPRODEV.App/Client/Program.cs
@@ -22,7 +22,8 @@ namespace BlazorREPRODEV.App.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://reprodevhost.azurewebsites.net") });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://reprodevhost.azurewebsites.net") });
             builder.Services.AddMudServices(config =>
             {
                 config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
